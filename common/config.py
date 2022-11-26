1e33683137cfa08205385401e9ec16ff12735f52
Breeze <96935062+br33z3s@users.noreply.github.com>
2022-11-27 01:48:26 +0800

    Delete config.py
    del config

--- common/config.py
+++
@@ -1,9 +0,0 @@
-class Config(object):
-    DEBUG = False
-    DB_SERVER = 'localhost:5432'
-    ENV = 'development'
-    DATABASE_URI = 'postgresql://breeze:225579qq@{}/breeze_video'.format(DB_SERVER)
-
-    # @property
-    # def DATABASE_URI(self):
-    #     return
